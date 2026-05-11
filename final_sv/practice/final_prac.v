4b0e63ebe9085e98c430ff7b2e0b16b8a3d6ab05
KPYerawar <krushnayerawar2004@gmail.com>
2026-05-11 19:32:27 +0530

    some practice done

---
+++ final_sv/practice/final_prac.v
@@ -0,0 +1,33 @@
+module final_prac;
+/*
+logic [7:0] data ;
+logic enable ;
+logic [8:0] double ;
+
+initial begin 
+data = 8'b110011;
+enale = 0;
+double = data << 1;
+#10;
+$display ("data = %d , enable = %d , double = %d , ", data , enable , double );
+$finish ;
+end 
+*/
+//data types 
+int a ;
+bit b;
+bit [3:0] c;
+byte d;
+longint = e;
+
+initial begin 
+a = 3;
+b = 1 ;
+c = 4'b0011;
+d = 8'hbc;
+e = 64'b110011001100;
+#20;
+$display ("a = %d, b = %d , c = %d ,d = %d,e=  %d ,", a, b, c , d , e);
+end 
+
+endmodule 
